6904f74a813ceafcfdb89cd64a12d5c165f11ee5
Admin <Admin@DESKTOP-MENGJQL>
2023-08-10 15:25:38 +0530

    Additional data

---
+++ DevopsProject/src/com/lbrce/Devops/test2.java
@@ -0,0 +1,10 @@
+package com.lbrce.Devops;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Devops additional data");
+	}
+
+}
